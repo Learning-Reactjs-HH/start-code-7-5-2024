f3c6198356e15bd8a57f4942f0ece44e57573600
Hoàng <hoanghung20030@gmail.com>
2024-05-07 19:41:00 +0700

    exercise 3

--- exercice-3/main.js
+++ exercice-3/main.js
@@ -1,8 +1,8 @@
 //
 //  In this challenge you need
 //
-//  1- To init npm in this project, by typing npm init
-//  2- To install a new npm pacakge named "colors"
+//  1- To init npm in this project, by typing npm init   // npm i
+//  2- To install a new npm pacakge named "colors"   // npm i colors
 //  3- To important this package in this file (main.js)
 //  4- Dont forget also to update the package.json file with the following line : "type": "module"
 
@@ -10,5 +10,5 @@
 // Want know more about this package?      https://www.npmjs.com/package/colors
 
 // For fun, display you best logs usin this pacakge
-
+import colors from 'colors';
 console.log(colors.red("Hello world!"));
